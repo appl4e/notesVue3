b1c8be1a1299bd9e16584dcb612d59a9d0db52ad
Apple <apple@redlimesolutions.com>
2022-02-16 11:04:57 +0600

    linting added and removed unused codes files

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+const hi = "Apple";
 </script>
 
 <template>
-  <h1>Hello World test</h1>
+  <h1>Hello World test {{ hi }}</h1>
 </template>
 
